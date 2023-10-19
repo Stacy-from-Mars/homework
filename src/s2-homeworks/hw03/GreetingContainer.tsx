c29b936ac130bbeedc488f8b24792bd90e4548c0
Stacy <stacy.from.mars@gmail.com>
2023-10-19 22:54:15 +0300

    testing commit 86

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -52,7 +52,6 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
     const onEnter = (e: any) => {
         pureOnEnter(e, addUser)
     }
-
     const totalUsers = users.length // need to fix
     const lastUserName = users.length > 0 ? users[users.length - 1].name : "" // need to fix
 
