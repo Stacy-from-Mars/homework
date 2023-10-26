0b245b6b169194cec7c9448444460e2f37c55fa9
Stacy <stacy.from.mars@gmail.com>
2023-10-27 01:16:32 +0300

    test commit 66

--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -3,7 +3,6 @@ import s from './Greeting.module.css'
 
 type GreetingPropsType = {
     name: string // need to fix any
-
     setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void// need to fix any
     addUser: () => void // need to fix any
 
