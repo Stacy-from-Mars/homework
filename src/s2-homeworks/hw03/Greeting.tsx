f3293edf3e52a094e397b6c4e3b64fed3baec5b3
Stacy <stacy.from.mars@gmail.com>
2023-11-15 02:34:24 +0300

    test commit 182

--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -11,6 +11,7 @@ type GreetingPropsType = {
     onEnter: (e: any) => void // need to fix any
 
     error: string // need to fix any
+
     totalUsers: number // need to fix any
     lastUserName?: string // need to fix any
 }
