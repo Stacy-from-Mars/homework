62cd2e9703337c177ea880710ca28becfbfe470d
Stacy <stacy.from.mars@gmail.com>
2023-11-28 22:32:46 +0300

    test commit 233

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -42,6 +42,7 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
 
     const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
         setName(e.currentTarget.value) // need to fix
+
         error && setError('')
     }
 
