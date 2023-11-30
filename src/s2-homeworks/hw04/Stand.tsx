e4bb7a5889d54a66b600a4a3d3e2fcca2c392685
Stacy <stacy.from.mars@gmail.com>
2023-12-01 00:08:42 +0300

    test commit 245

--- src/s2-homeworks/hw04/Stand.tsx
+++ src/s2-homeworks/hw04/Stand.tsx
@@ -12,6 +12,7 @@ const Stand = () => {
 
     return (
         <div id={'hw4-stand'} className={s.stand}>
+
             <div className={s.inputs}>
                 {/*совместим со старым кодом:*/}
                 <div>
