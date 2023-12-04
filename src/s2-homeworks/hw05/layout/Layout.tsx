d5c512d14771dcfcb4da85b3e37a5947e684e276
Stacy <stacy.from.mars@gmail.com>
2023-12-05 00:05:19 +0300

    test commit 264

--- src/s2-homeworks/hw05/layout/Layout.tsx
+++ src/s2-homeworks/hw05/layout/Layout.tsx
@@ -8,7 +8,6 @@ type PropsType = {
 
 export const Layout: FC<PropsType> = ({ children }) => {
     const [open, setOpen] = useState(false)
-
     const handleClose = () => setOpen(false)
     const handleOpen = () => setOpen(true)
 
