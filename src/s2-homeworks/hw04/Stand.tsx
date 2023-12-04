1e750103cc9737145ccbe491188530fc4982e83d
Stacy <stacy.from.mars@gmail.com>
2023-12-04 22:23:35 +0300

    test commit 259

--- src/s2-homeworks/hw04/Stand.tsx
+++ src/s2-homeworks/hw04/Stand.tsx
@@ -35,6 +35,7 @@ const Stand = () => {
                                     : 'Error'
                             )
                             setValue('')
+
                         }}
                     />
                 </div>
