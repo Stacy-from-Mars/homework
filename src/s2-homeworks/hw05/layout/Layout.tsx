6cc1846366860c797f3aa6d53e653e824bab763f
Stacy <stacy.from.mars@gmail.com>
2023-12-11 20:03:25 +0300

    test commit 291

--- src/s2-homeworks/hw05/layout/Layout.tsx
+++ src/s2-homeworks/hw05/layout/Layout.tsx
@@ -19,6 +19,7 @@ export const Layout: FC<PropsType> = ({ children }) => {
     return (
         <>
             <Sidebar open={open} handleClose={handleClose} />
+
             <Header handleOpen={handleOpen} />
             <div>
                 {/*страницы*/}
