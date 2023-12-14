63b4eae2e2589dbf8e2fcb978b2d725589cedd19
Stacy <stacy.from.mars@gmail.com>
2023-12-14 22:47:41 +0300

    test commit 305

--- src/s2-homeworks/hw05/layout/Layout.tsx
+++ src/s2-homeworks/hw05/layout/Layout.tsx
@@ -13,7 +13,6 @@ export const Layout: FC<PropsType> = ({ children }) => {
 
     useEffect(() => {
         open && (document.body.style.overflow = 'hidden')
-
         !open && (document.body.style.overflow = 'unset')
 
     }, [open]) // отключает прокрутку при открытом меню
